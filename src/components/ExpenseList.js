d29f31f15b008c118fd2593646ae89c1e67eeef1
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-02-09 16:54:31 -0500

    Edits and refactoring.

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,15 +3,15 @@ import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
-    const { expenses } = React.useContext(AppContext);
+	const { expenses } = React.useContext(AppContext);
 
-    return (
-        <ul className='list-group'>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
-        </ul>
-    );
+	return (
+		<ul className='list-group'>
+			{expenses.map((expense) => (
+				<ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+			))}
+		</ul>
+	);
 };
 
 export default ExpenseList;
