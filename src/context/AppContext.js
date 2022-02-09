63703db6bcf94faf731264a3024ed1ca9e6a0f2d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-02-09 17:01:03 -0500

    Edits and refactoring.

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const AppReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_EXPENSE':
-            return {
-                ...state,
-                expenses: [...state.expenses, action.payload],
-            };
-        default: 
-        return state;
-    }
-}
+	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		default:
+			return state;
+	}
+};
 
 const initialState = {
     budget: 2000,
@@ -24,17 +24,17 @@ const initialState = {
 export const AppContext = React.createContext();
 
 export const AppProvider = (props) => {
-    const [state, dispatch] = React.useReducer(AppReducer, initialState);
+	const [state, dispatch] = React.useReducer(AppReducer, initialState);
 
-    return (
-        <AppContext.Provider
-            value={{
-                budget: state.budget,
-                expenses: state.expenses,
-                dispatch,
-            }}
-        >
-            {props.children}
-        </AppContext.Provider>
-    );
+	return (
+		<AppContext.Provider
+			value={{
+				budget: state.budget,
+				expenses: state.expenses,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
 };
