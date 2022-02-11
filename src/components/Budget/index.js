75b81a008aef47c9d805c4a61814eac04c2cec0d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-02-11 12:01:47 -0500

    File structure optimization.

--- src/components/Budget/index.js
+++ src/components/Budget/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppContext } from '../context/AppContext';
+import { AppContext } from '../../context/AppContext';
 
 const Budget = () => {
 	const { budget } = React.useContext(AppContext);
