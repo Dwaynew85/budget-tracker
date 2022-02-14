2a807c3877457d95529d2fcb8007bfe8a8a4a6f6
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-02-14 11:19:19 -0500

    Added component to show budget and edit button.

---
+++ src/components/ViewBudget/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ViewBudget = ({ budget, handleEditClick }) => {
+    return (
+        <>
+            <span>Budget: ${budget}</span>
+            <button
+                type='button'
+                class='btn btn-primary'
+                onClick={handleEditClick}
+            >
+                Edit
+            </button>
+        </>
+    );
+};
+
+export default ViewBudget;
